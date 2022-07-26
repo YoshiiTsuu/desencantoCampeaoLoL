c228f7f9af10896f4033d4859c5281c9f677e074
Yoshii <krk.miya@gmail.com>
2022-07-26 19:41:20 -0300

    Alterando para que a função desencante os campeões do LoL

--- paintBot/main.py
+++ paintBot/main.py
@@ -1,24 +1,25 @@
 import pyautogui
-
-#pyautogui.hotkey('alt', 'tab')
-
 import pyautogui, sys
-print('Press Ctrl-C to quit.')
-try:
-    while True:
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
-except KeyboardInterrupt:
-    print('\n')
+import time
 
-# i = 0;
-# while i< 5:
-#         pyautogui.click(3200,410);
-#         pyautogui.click(4326,944);
-#         pyautogui.click(4289,1031);
-#         i+=1;
+# print('Press Ctrl-C to quit.')
+# try:
+#     while True:
+#         x, y = pyautogui.position()
+#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#         print(positionStr, end='')
+#         print('\b' * len(positionStr), end='', flush=True)
+# except KeyboardInterrupt:
+#     print('\n')
+
+i = 0;
+while i< 4:
+        pyautogui.click(430,414);
+        time.sleep(1);
+        pyautogui.click(661,450);
+        time.sleep(1);
+        pyautogui.click(848,673);
+        i+=1;
 
 
 
